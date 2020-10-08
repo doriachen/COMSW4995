790a2e274257a21eb1b69d13d8c9a89c73b52e13
Doria Chen <37597479+doriachen@users.noreply.github.com>
2020-10-08 02:56:23 -0400

    added a new class?

--- my_test_proj/map.py
+++ my_test_proj/map.py
@@ -4,3 +4,12 @@ def searchbar():
     searchvalue = input("Search:\n")
     #print(f'Results for {value}')
     return searchvalue;
+
+class Store:
+  def __init__(self, name, location, description):
+    self.name = name
+    self.location = location
+    self.description = description
+
+p1 = Store("Westside", "Morningside Heights", "Grocery store")
+
