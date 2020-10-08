d85fd593ca3d213389bd12190028b7454966bb51
Doria Chen <37597479+doriachen@users.noreply.github.com>
2020-10-08 03:36:35 -0400

    Update map.py

--- my_test_proj/map.py
+++ my_test_proj/map.py
@@ -1,13 +1,19 @@
 #some fake code for our project!
 import numpy
 
+#example code from prof paine
 def inc(x):
     return x + 1
+
+#searchbar method that should only accept strings in search bar
 def searchbar():
     searchvalue = input("Search:\n")
-    #print(f'Results for {value}')
-    return searchvalue;
-
+    if isinstance(searchvalue, str)
+        return searchvalue
+    else:
+        print("Please enter a word")
+        searchbar()
+#defining a class for list objects?    
 class Biz:
   def __init__(self, name, location, description):
     self.name = name
@@ -20,3 +26,4 @@ p2 = Biz("Up", "Morningside Heights", "Coffee shop")
 #Make a multidimensional list to store shops/businesses? a nested array?
 Ls = [p1,p2]
 
+#write a method to force certain fields to be filled? and filled with certain types?
