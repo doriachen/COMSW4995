777b9ad9ddf6f98bc0cf423f009e71a2228ac5ad
Doria Chen <37597479+doriachen@users.noreply.github.com>
2020-10-08 02:39:28 -0400

    Update test_all.py

--- my_test_proj/tests/test_all.py
+++ my_test_proj/tests/test_all.py
@@ -1,6 +1,14 @@
 #import the method from project folder
 from my_test_proj import inc
 
-#define test method
+#define test method for inc
 def test_inc():
     assert inc(1) == 2
+    
+#test 1 - takes in a string for name of store
+def test_shopname():
+    name = isinstance(getname(x), str)
+    if name:
+        print("Pass test 1")
+
+#test 2
