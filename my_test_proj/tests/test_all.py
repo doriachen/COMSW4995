fd93caedf2a8e8b2ea0acee024704f2e7d5f0050
Doria Chen <37597479+doriachen@users.noreply.github.com>
2020-10-08 02:51:44 -0400

    updated search test

--- my_test_proj/tests/test_all.py
+++ my_test_proj/tests/test_all.py
@@ -1,14 +1,27 @@
 #import the method from project folder
 from my_test_proj import inc
+from my_test_proj import searchbar
+
 
 #define test method for inc
 def test_inc():
-    assert inc(1) == 2
+    assert inc(1) == 2, "did not pass test_inc"
     
-#test 1 - takes in a string for name of store
-def test_shopname():
-    name = isinstance(getname(x), str)
-    if name:
+#test 1 - takes in a string from searchbar
+def test_searchbar():
+    searchedvalue = isinstance(searchbar(), str)
+    if searchedvalue:
         print("Pass test 1")
+    else
+        print("fail test 1")
 
-#test 2
+#test 2 - 
+
+
+#test 3 -
+
+
+#test 4 - 
+
+
+#test 5 - 
