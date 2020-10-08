e2ac0e168fcd20085889bde59676c3dd19a91d25
Doria Chen <37597479+doriachen@users.noreply.github.com>
2020-10-08 03:18:58 -0400

    initialized example list

--- my_test_proj/map.py
+++ my_test_proj/map.py
@@ -1,3 +1,5 @@
+import numpy
+
 def inc(x):
     return x + 1
 def searchbar():
@@ -5,11 +7,15 @@ def searchbar():
     #print(f'Results for {value}')
     return searchvalue;
 
-class Store:
+class Biz:
   def __init__(self, name, location, description):
     self.name = name
     self.location = location
     self.description = description
 
-p1 = Store("Westside", "Morningside Heights", "Grocery store")
+p1 = Biz("Westside", "Morningside Heights", "Grocery store")
+p2 = Biz("Up", "Morningside Heights", "Coffee shop")
+
+#Make a multidimensional list to store shops/businesses? a nested array?
+Ls = [p1,p2]
 
