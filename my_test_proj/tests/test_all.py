29d5108406c31943ff13d4913cb3d4cedc923f96
Doria Chen <37597479+doriachen@users.noreply.github.com>
2020-10-08 03:26:35 -0400

    added a few test options

--- my_test_proj/tests/test_all.py
+++ my_test_proj/tests/test_all.py
@@ -13,15 +13,23 @@ def test_searchbar():
     if searchedvalue:
         print("Pass test 1")
     else
-        print("fail test 1")
+        print("Fail test 1")
 
-#test 2 - 
+#test 2 - check if duplicates in list
+def anydup(thelist):
+  seen = set()
+  for x in thelist:
+    if x in seen: print("Fail test 2")
+    seen.add(x)
+  return print("Pass test 2")
+    
+
+#test 3 - check if a list item has missing info from specific fields
 
 
-#test 3 -
-
-
-#test 4 - 
-
+#test 4 - check if list is empty
+def emptyList(thelist):
+    if not a:
+        print("Fail test 3)
 
 #test 5 - 
